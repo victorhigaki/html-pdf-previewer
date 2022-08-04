67cf0f02bade2bf407f94e0acd580ad69dd7fc3d
victorhigaki <victorhigaki@outlook.com>
2022-08-04 00:40:22 -0300

    feat: preview of html to pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+declare var require: any;
+const htmlToPdfmake = require('html-to-pdfmake');
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'html-to-pdf';
+  htmlString: string = '';
+
+  constructor() {}
+
+  save() {
+    const preview = document.getElementById('preview');
+    if (preview) preview.innerHTML = this.htmlString;
+  }
+
+  showPdf() {
+    const printPreview = document.getElementById(
+      'printPreview'
+    ) as HTMLIFrameElement;
+    var html = htmlToPdfmake(this.htmlString);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).getBase64(
+      function (encodedString: any) {
+        printPreview.src = 'data:application/pdf;base64,' + encodedString;
+      }.bind(this)
+    );
+  }
 }
